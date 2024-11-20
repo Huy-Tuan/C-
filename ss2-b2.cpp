5793d556758fdeb16eb78e58b7c321886eedd66e
Huy-Tuan <sieuka1990@gmail.com>
2024-11-20 18:37:09 +0700

    Add files via upload

---
+++ ss2-b2.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+	
+	int a, b, tong;
+	printf("Moi ban nhap vao so a \n");
+	scanf("%d",&a);
+	
+	printf("Moi ban nhap vao so b \n");
+	scanf("%d",&b);
+	
+	tong = a + b;
+	printf("Tong cua a + b = %d",tong);
+	
+	return 0;
+}
