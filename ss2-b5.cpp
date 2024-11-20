ff53ac3169d0321c24361df8b071add052b4e26e
Huy-Tuan <sieuka1990@gmail.com>
2024-11-20 18:50:09 +0700

    Add files via upload

---
+++ ss2-b5.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main () {
+	float dai = 5, rong = 3, chuvi, dientich;
+	chuvi = (dai + rong) * 2;
+	dientich = dai*rong;
+	printf("Chu vi hinh chu nhat la %.2f\n",chuvi);
+	printf("Dien tich hinh chu nhat la %.2f",dientich);
+	
+	return 0;
+    
+}
