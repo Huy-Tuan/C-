d15e690779bf857646b6d5409be4ec790e4ca6b0
Huy-Tuan <sieuka1990@gmail.com>
2024-11-20 18:55:31 +0700

    Add files via upload

---
+++ ss2-b6.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+	
+	float Pi = 3.14, R = 5, chuvi, dientich;
+	chuvi = R * 2 * Pi;
+	dientich = Pi * R * R;
+	
+	printf("Chu vi hinh tron la %.2f\n",chuvi);
+	printf("Dien tich hinh tron la %.2f",dientich);
+	
+	return 0;
+	 
+}
