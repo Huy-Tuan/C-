18c0444e6a1a0af808d0eef527c7a536481b52a5
Huy-Tuan <sieuka1990@gmail.com>
2024-11-20 18:30:45 +0700

    Add files via upload

---
+++ ss2-b1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+	// khai bao bien so nguyen
+	int age;
+	// khai bao bien so thuc
+	float height;
+	// khai bao ki tu
+	char character;
+	// khai bao chuoi
+	char fullName[50];
+}
