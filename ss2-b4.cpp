6f29a72876188424ad524772c4025306e487775b
Huy-Tuan <sieuka1990@gmail.com>
2024-11-20 18:47:18 +0700

    Add files via upload

---
+++ ss2-b4.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main () {
+	float canh = 5.5, chuvi, dientich;
+	chuvi = canh * 4;
+	dientich = canh * canh;
+	printf("Chu vi hinh vuong la %.2f\n",chuvi);
+	printf("Dien tich hinh vuong la %.2f",dientich);
+	
+	return 0;
+    
+}
