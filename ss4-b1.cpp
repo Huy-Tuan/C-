5af563a290d4583a3ad7b63447d9696afe5f1d3d
Huy-Tuan <sieuka1990@gmail.com>
2024-11-25 02:28:04 +0700

    Add files via upload

---
+++ ss4-b1.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+	int n;
+	printf("Moi ban nhap mot so nguyen ");
+	scanf("%d",&n);
+	if(n > 0){
+		printf("Day la so duong");
+	}
+	else{
+		printf("Day la so am");
+	}
+}
